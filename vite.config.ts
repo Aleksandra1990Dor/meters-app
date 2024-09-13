5614eb25851ac90d4a15fa98fa6d72181634f566
Aleksandra Dorofeeva <crazyleksy@gmail.com>
2024-09-13 17:13:03 +0300

    fix: alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import url from 'url';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
